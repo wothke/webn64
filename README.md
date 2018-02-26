edf61a8af4c1aea903ab07818edf9253e243a877
wothke <tinyrsid@wothke.ch>
2018-02-26 01:18:47 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,72 @@
+# webN64
+
+Copyright (C) 2015 Juergen Wothke
+
+This is a JavaScript/WebAudio plugin of kode54's "LazyUsf2 - USF Decoder" (based on Mupen64plus). This 
+plugin is designed to work with my generic WebAudio ScriptProcessor music player (see separate project). 
+
+It allows to play "(Ultra) Nintendo 64 Sound" music (.USF/.MINIUSF2 files).
+
+A live demo of this program can be found here: http://www.wothke.ch/webn64/
+
+
+## Credits
+The project is based on: https://gitlab.kode54.net/kode54/lazyusf2 
+
+
+## Project
+It includes most of the original "USF Decoder" code including all the necessary 3rd party dependencies. All the "Web" specific 
+additions (i.e. the whole point of this project) are contained in the "emscripten" subfolder. Due to the unavailability of SSE2 
+and x86 assembly code support in EMSCRIPTEN the "cached interpreter" mode is used.) The main interface 
+between the JavaScript/WebAudio world and the original C code is the adapter.c file. Some patches were necessary 
+within the original codebase (these can be located by looking for EMSCRIPTEN if-defs). 
+
+
+## Howto build
+
+You'll need Emscripten (http://kripken.github.io/emscripten-site/docs/getting_started/downloads.html). The make script 
+is designed for use of emscripten version 1.37.29 (unless you want to create WebAssembly output, older versions might 
+also still work).
+
+The below instructions assume that the webn64 project folder has been moved into the main emscripten 
+installation folder (maybe not necessary) and that a command prompt has been opened within the 
+project's "emscripten" sub-folder, and that the Emscripten environment vars have been previously 
+set (run emsdk_env.bat).
+
+The Web version is then built using the makeEmscripten.bat that can be found in this folder. The 
+script will compile directly into the "emscripten/htdocs" example web folder, were it will create 
+the backend_n64.js library. (To create a clean-build you have to delete any previously built libs in the 
+'built' sub-folder!) The content of the "htdocs" can be tested by first copying it into some 
+document folder of a web server. 
+
+
+## Depencencies
+
+The current version requires version 1.02 (older versions will not
+support WebAssembly) of my https://github.com/wothke/webaudio-player.
+
+This project comes without any music files, so you'll also have to get your own and place them
+in the htdocs/music folder (you can configure them in the 'songs' list in index.html).
+
+
+## Disclaimer
+
+This emulator is not as stable as I would like and there are songs which actually manage to crash 
+it. From what I've read there does not seem to be one perfect Nintendo64 emulator and what works with 
+one may fail with the next (and vice versa). And who knows, maybe future improvements will make this 
+one more stable.
+
+
+## License
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version. This library is distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
---
+++ emscripten/n64_adapter.js
@@ -0,0 +1,145 @@
+/*
+ n64_adapter.js: Adapts LazyUSF backend to generic WebAudio/ScriptProcessor player.
+ 
+ version 1.0
+ 
+ 	Copyright (C) 2018 Juergen Wothke
+
+ LICENSE
+ 
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2.1 of the License, or (at
+ your option) any later version. This library is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+N64BackendAdapter = (function(){ var $this = function () {
+		$this.base.call(this, backend_N64.Module, 2);
+		this._manualSetupComplete= true;
+		this._undefined;
+		this._currentPath;
+		this._currentFile;
+		
+		if (!backend_N64.Module.notReady) {
+			// in sync scenario the "onRuntimeInitialized" has already fired before execution gets here,
+			// i.e. it has to be called explicitly here (in async scenario "onRuntimeInitialized" will trigger
+			// the call directly)
+			this.doOnAdapterReady();
+		}				
+	}; 
+	// sample buffer contains 2-byte integer sample data (i.e. 
+	// must be rescaled) of 2 interleaved channels
+	extend(EmsHEAP16BackendAdapter, $this, {
+		doOnAdapterReady: function() {
+			// called when runtime is ready (e.g. asynchronously when WASM is loaded)
+			// if FS needed to be setup of would be done here..
+		},
+		getAudioBuffer: function() {
+			var ptr=  this.Module.ccall('emu_get_audio_buffer', 'number');			
+			// make it a this.Module.HEAP16 pointer
+			return ptr >> 1;	// 2 x 16 bit samples			
+		},
+		getAudioBufferLength: function() {
+			var len= this.Module.ccall('emu_get_audio_buffer_length', 'number');
+			return len;
+		},
+		computeAudioSamples: function() {
+			return this.Module.ccall('emu_compute_audio_samples', 'number');
+		},
+		getMaxPlaybackPosition: function() { 
+			return this.Module.ccall('emu_get_max_position', 'number');
+		},
+		getPlaybackPosition: function() {
+			return this.Module.ccall('emu_get_current_position', 'number');
+		},
+		seekPlaybackPosition: function(pos) {
+			var current= this.getPlaybackPosition();
+			if (pos < current) {
+				// hack: for some reason backward seeking fails ('he: execution error') if "built-in"
+				// file reload if used... 
+				var ret = this.Module.ccall('emu_init', 'number', 
+							['string', 'string'], 
+							[ this._currentPath, this._currentFile]);
+			}
+			this.Module.ccall('emu_seek_position', 'number', ['number'], [pos]);
+		},
+		
+		// XXX FIXME load from same path as initial file..
+		getPathAndFilename: function(filename) {
+			var sp = filename.split('/');
+			var fn = sp[sp.length-1];					
+			var path= filename.substring(0, filename.lastIndexOf("/"));	
+			if (path.lenght) path= path+"/";
+			
+			return [path, fn];
+		},
+		
+		mapBackendFilename: function (name) {
+			// "name" comes from the C++ side 
+			var input= this.Module.Pointer_stringify(name);
+			return input;
+		},
+		registerFileData: function(pathFilenameArray, data) {
+			return this.registerEmscriptenFileData(pathFilenameArray, data);
+		},
+		loadMusicData: function(sampleRate, path, filename, data, options) {
+			var ret = this.Module.ccall('emu_init', 'number', 
+								['string', 'string'], 
+								[ path, filename]);
+
+			if (ret == 0) {
+				var inputSampleRate = this.Module.ccall('emu_get_sample_rate', 'number');
+				this.resetSampleRate(sampleRate, inputSampleRate); 
+				this._currentPath= path;
+				this._currentFile= filename;
+			} else {
+				this._currentPath= this._undefined;
+				this._currentFile= this._undefined;
+			}
+			return ret;
+		},
+		evalTrackOptions: function(options) {
+// is there any scenario with subsongs?
+			if (typeof options.timeout != 'undefined') {
+				ScriptNodePlayer.getInstance().setPlaybackTimeout(options.timeout*1000);
+			}
+			var id= (options && options.track) ? options.track : 0;		
+			var boostVolume= (options && options.boostVolume) ? options.boostVolume : 0;		
+			return this.Module.ccall('emu_set_subsong', 'number', ['number', 'number'], [id, boostVolume]);
+		},				
+		teardown: function() {
+			this.Module.ccall('emu_teardown', 'number');	// just in case
+		},
+		getSongInfoMeta: function() {
+			return {title: String,
+					artist: String, 
+					game: String, 
+					year: String, 
+					genre: String,
+					copyright: String,
+					psfby: String
+					};
+		},
+		
+		updateSongInfo: function(filename, result) {
+			var numAttr= 7;
+			var ret = this.Module.ccall('emu_get_track_info', 'number');
+
+			var array = this.Module.HEAP32.subarray(ret>>2, (ret>>2)+numAttr);
+			result.title= this.Module.Pointer_stringify(array[0]);
+			if (!result.title.length) result.title= filename;		
+			result.artist= this.Module.Pointer_stringify(array[1]);		
+			result.game= this.Module.Pointer_stringify(array[2]);
+			result.year= this.Module.Pointer_stringify(array[3]);
+			result.genre= this.Module.Pointer_stringify(array[4]);
+			result.copyright= this.Module.Pointer_stringify(array[5]);
+			result.psfby= this.Module.Pointer_stringify(array[6]);
+		}
+	});	return $this; })();
